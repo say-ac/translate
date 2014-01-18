e9d6f6174e1a38253a20af5f44c51dc39b4592c6
sfyigit <sezerfatih07@gmail.com>
2014-01-18 13:48:29 +0200

    Update geri_bildirim_lang.php

--- en/geri_bildirim_lang.php
+++ en/geri_bildirim_lang.php
@@ -1,11 +1,12 @@
 <?php
 
-$lang['geri_bildirim_baslik'] = 'Geri Bildirim';
-$lang['geri_bildirim_bilgi'] = 'Fikir ve düşünceleriniz bizim için önemlidir.';
-$lang['geri_bildirim_adiniz_ve_soyadiniz'] = 'Adınız ve soyadınız';
-$lang['geri_bildirim_adiniz_ve_soyadiniz_aciklama'] = 'Adınızı ve soyadınızı giriniz.';
-$lang['geri_bildirim_mail_adresiniz'] = 'Mail adresiniz';
-$lang['geri_bildirim_mail_adresiniz_aciklama'] = 'Mail adresinizi yazınız.';
-$lang['geri_bildirim_mesajiniz'] = 'Mesajınız';
-$lang['geri_bildirim_mesajiniz_aciklama'] = 'Lütfen mesajınızı detaylı bir şekilde yazınız.';
-$lang['geri_bildirim_gonder'] = 'Gönder';
+$lang['geri_bildirim_baslik'] = 'Feedback';
+$lang['geri_bildirim_bilgi'] = 'Your ideas and thoughts are important for us.';
+$lang['geri_bildirim_adiniz_ve_soyadiniz'] = 'Name and Surname';
+$lang['geri_bildirim_adiniz_ve_soyadiniz_aciklama'] = 'Enter your name and surname.';
+$lang['geri_bildirim_mail_adresiniz'] = 'Mail Address';
+$lang['geri_bildirim_mail_adresiniz_aciklama'] = 'Enter your mail address.';
+$lang['geri_bildirim_mesajiniz'] = 'Your message';
+$lang['geri_bildirim_mesajiniz_aciklama'] = 'Please write your messege detailed. ';
+$lang['geri_bildirim_gonder'] = 'Send';
+
