d92886dfaed1651862e1c70fbadaa248441f7dad
sfyigit <sezerfatih07@gmail.com>
2014-01-18 13:53:17 +0200

    Update ilanlar_lang.php

--- en/ilanlar_lang.php
+++ en/ilanlar_lang.php
@@ -1,15 +1,15 @@
 <?php
 
-$lang['ilanlar_baslik'] = 'İlanlar';
-$lang['ilanlar_yeni_ilan'] = 'Yeni İlan';
-$lang['ilanlar_bilgi'] = 'İlanlarda sunucu zamanı dikkate alınmaktadır. Sunucu zamanı sayfanın alt kısmında gösterilmektedir.';
-$lang['ilanlar_kategoriler'] = 'Kategoriler';
-$lang['ilanlar_tablo_ekleyen'] = 'Ekleyen';
-$lang['ilanlar_tablo_baslik'] = 'Başlık';
-$lang['ilanlar_one_cikan'] = 'öne çıkan';
+$lang['ilanlar_baslik'] = 'Advertisements';
+$lang['ilanlar_yeni_ilan'] = 'New Advertisement';
+$lang['ilanlar_bilgi'] = 'At the ads server time is considered. You can see the server time at he bottom of the page.';
+$lang['ilanlar_kategoriler'] = 'Categories';
+$lang['ilanlar_tablo_ekleyen'] = 'Submitted by';
+$lang['ilanlar_tablo_baslik'] = 'Title';
+$lang['ilanlar_one_cikan'] = 'Highlights';
 
 // 1 yorum
-$lang['ilanlar_yorum'] = 'yorum';
+$lang['ilanlar_yorum'] = 'comment';
 
 // 3 yorum, ingilizce için comments olabilir
-$lang['ilanlar_yorumlar'] = 'yorum';
+$lang['ilanlar_yorumlar'] = 'comments';
