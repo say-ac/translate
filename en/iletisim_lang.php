21873aff16256bce06b5ee7825c7637480811932
sfyigit <sezerfatih07@gmail.com>
2014-01-18 13:56:00 +0200

    Update iletisim_lang.php

--- en/iletisim_lang.php
+++ en/iletisim_lang.php
@@ -1,11 +1,11 @@
 <?php
 
-$lang['iletisim_baslik'] = 'İletişim';
-$lang['iletisim_bilgi'] = 'Her türlü görüş, öneri, istek, şikayet vs. konularında mesaj yazabilirsiniz.';
-$lang['iletisim_adiniz_ve_soyadiniz'] = 'Adınız ve Soyadınız';
-$lang['iletisim_adiniz_ve_soyadiniz_aciklama'] = 'Adınızı giriniz.';
-$lang['iletisim_mail_adresiniz'] = 'Mail Adresiniz';
-$lang['iletisim_mail_adresiniz_aciklama'] = 'Mail adresinizi yazınız.';
-$lang['iletisim_mesajiniz'] = 'Mesajınız';
-$lang['iletisim_mesajiniz_aciklama'] = 'Mesajınızı yazınız.';
-$lang['iletisim_gonder'] = 'Gönder';
+$lang['iletisim_baslik'] = 'Communication';
+$lang['iletisim_bilgi'] = 'You can write message about any ideas, suggestions, requests, complaints, etc.';
+$lang['iletisim_adiniz_ve_soyadiniz'] = 'Name and surname';
+$lang['iletisim_adiniz_ve_soyadiniz_aciklama'] = 'Enter your name.';
+$lang['iletisim_mail_adresiniz'] = 'Mail address';
+$lang['iletisim_mail_adresiniz_aciklama'] = 'Enter your mail address.';
+$lang['iletisim_mesajiniz'] = 'Your message';
+$lang['iletisim_mesajiniz_aciklama'] = 'Write your message.';
+$lang['iletisim_gonder'] = 'Send';
